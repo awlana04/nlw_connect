a624acb442e75a784d56d83a1570fc933bc74612
awlana04 <cawlana040@gmail.com>
2025-02-20 22:31:49 -0300

    validators added

---
+++ src/validators/subscribers_creator_validator.py
@@ -0,0 +1,14 @@
+from cerberus import Validator
+
+def subscribers_creator_validator(request: any):
+  body_validator = Validator({
+    "data": {
+      "type": "dict",
+      "schema": {
+        "name": {"type": "string", "required": True, "empty": False},
+        "email": {"type": "string", "required": True, "empty": False},
+        "link": {"type": "string", "required": False, "empty": False},
+        "event_id": {"type": "integer", "required": True, "empty": False},
+      }
+    }
+  })
