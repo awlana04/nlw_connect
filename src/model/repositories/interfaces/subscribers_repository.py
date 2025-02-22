5d9298589b30f894260d65e4d5f10e3ecc8bdd07
awlana04 <cawlana040@gmail.com>
2025-02-21 23:05:02 -0300

    added new methods in subscribers repository

--- src/model/repositories/interfaces/subscribers_repository.py
+++ src/model/repositories/interfaces/subscribers_repository.py
@@ -8,3 +8,9 @@ class SubscribersRepositoryInterface:
 
   @abstractmethod
   def select_subscriber(self, email: str, event_id: int) -> Subscribers: pass
+
+  @abstractmethod
+  def select_subscribers_by_link(self, link: str, event_id: int) -> list: pass
+
+  @abstractmethod
+  def get_ranking(self, evnet_id: int) -> list: pass
--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -22,3 +22,13 @@ def test_select_subscriber():
   response = subscriber_repository.select_subscriber(email, event_id)
 
   print(response.name)
+
+@pytest.mark.skip("Select in DB")
+def test_ranking():
+  event_id = 1
+
+  subscriber_repository = SubscribersRepository()
+  response = subscriber_repository.get_ranking(event_id)
+
+  for element in response:
+    print(f"Link: {element.link}, total de inscritos: {element.total}")
